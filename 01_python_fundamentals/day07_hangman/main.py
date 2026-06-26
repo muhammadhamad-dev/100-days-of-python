508f1d0deb4056286a0af2cafaf17bd056d62540
hamad <09-132242-026@student.bahria.edu.pk>
2026-06-26 15:59:35 +0500

    Update task 7 code and renamed it to main

---
+++ 01_python_fundamentals/day07_hangman/main.py
@@ -0,0 +1,48 @@
+import random
+import hangman_words
+import hangman_art
+
+word_list = hangman_words.word_list
+lives = 6
+
+print(hangman_art.logo)
+chosen_word = random.choice(word_list)
+
+placeholder = "_" * len(chosen_word)
+print("Word to guess: " + placeholder)
+
+game_over = False
+correct_letters = []
+
+while not game_over:
+    print(f"****************************<???>/{lives} LIVES LEFT****************************")
+    guess = input("Guess a letter: ").lower()
+
+    if guess in correct_letters:
+        print(f"You've already guessed Letter {guess}")
+
+    display = ""
+    for letter in chosen_word:
+        if letter == guess:
+            display += letter
+            correct_letters.append(guess)
+        elif letter in correct_letters:
+            display += letter
+        else:
+            display += "_"
+
+    print("Word to guess: " + display)
+
+    if guess not in chosen_word:
+        lives -= 1
+        print(f"You've guessed {guess} and it's not in the word. You lose a life.")
+
+        if lives == 0:
+            game_over = True
+            print(f"***********************YOU LOSE! The correct word was {chosen_word}**********************")
+
+    if "_" not in display:
+        game_over = True
+        print("****************************YOU WIN****************************")
+
+    print(hangman_art.stages[lives])
